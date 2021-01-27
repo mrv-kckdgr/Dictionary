9d456abb3a720a90524e875ba2c60bc079e01706
KucukdogruMerve <33333935+Kucukdogru@users.noreply.github.com>
2021-01-27 12:44:26 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKey = keys;
+            keys = new TKey[keys.Length + 1];
+            
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+            }
+
+            TValue[] tempValue = values;
+            values = new TValue[values.Length + 1];
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                values[i] = tempValue[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+            
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " : ");
+            }
+
+            for (int j = 0; j < values.Length; j++)
+            {
+                Console.WriteLine(values[j]);
+            }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> customerInformation = new MyDictionary<string, string>();
+            customerInformation.Add("names", "Merve");
+            customerInformation.Add("surnames", "KÜÇÜKDOĞRU");
+            customerInformation.Add("job", "Computer Engineer");
+
+            customerInformation.List();
+        }
+    }
+}
